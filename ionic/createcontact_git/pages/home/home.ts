1a25446b6fa988fe8788639f5a146685f6b88e77
mohamadem60mdem <mohamadem60@yahoo.com>
2018-06-30 19:53:03 -0400

    dff
    ftyfj

---
+++ ionic/createcontact_git/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(private contacts: Contacts,public navCtrl: NavController) {
+
+  }
+
+  s1(){
+    let contact: Contact = this.contacts.create();
+
+    contact.name = new ContactName(null, 'Smith', 'John');
+    contact.phoneNumbers = [new ContactField('mobile', '6471234567')];
+    contact.save().then(
+      () => console.log('Contact saved!', contact),
+      (error: any) => console.error('Error saving contact.', error)
+    );
+    
+  }
+
+}
